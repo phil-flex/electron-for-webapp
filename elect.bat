7bb7250e4ba866c0816ec81e6bf8c551e441ce48
phil <phil@flexsystem.com>
2018-12-06 11:26:25 +0800

    run electron in background

--- elect.bat
+++ elect.bat
@@ -1,3 +1,3 @@
 @@echo off
-REM start electron --preload=d:\Tools\electron2\resources\app\src\electron-main.js https://flexhk.sky-computers.com/%1
-start c:\Tools\electron\electron %1
+REM start /B electron --preload=d:\Tools\electron2\resources\app\src\electron-main.js https://flexhk.sky-computers.com/%1
+start /B c:\Tools\electron\electron %1
